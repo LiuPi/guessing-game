c240464df4375c1fbbab1fb6de7eae44208e0e55
Liudmila Pishchykava <mizuxira@yandex.ru>
2017-02-05 00:53:12 +0300

    guessing-game/feature: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.guessNum = 0;
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNum = Math.ceil((this.max+this.min)/2);
+        return this.guessNum;
     }
 
     lower() {
-
+        this.max = this.guessNum;
     }
 
     greater() {
-
+        this.min = this.guessNum;
     }
 }
 
